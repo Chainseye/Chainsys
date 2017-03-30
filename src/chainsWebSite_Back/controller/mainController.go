5b7728c7a4635a70012940707c913e8f56969d7b
liph <liph@hiscene.com>
2017-03-30 15:44:11 +0800

    remove unuseful fmt.println & add necessary file & add go net

--- src/chainsWebSite_Back/controller/mainController.go
+++ src/chainsWebSite_Back/controller/mainController.go
@@ -7,8 +7,6 @@ import (
 
 func Index(w http.ResponseWriter, r *http.Request) {
 	// 向客户端写入内容
-	fmt.Println("////////////////")
 	fmt.Fprintf(w, "Hello World! \n")
-	fmt.Println("aaaa")
 	Mysql_Driver_test()
 }
--- src/chainsWebSite_Back/main.go
+++ src/chainsWebSite_Back/main.go
@@ -14,5 +14,4 @@ func main() {
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
-	fmt.Println("end")
 }
