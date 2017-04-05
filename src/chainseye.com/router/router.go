e8cac86c512074362ee10a6645f4e779668e008a
liph <liph@hiscene.com>
2017-04-05 19:02:21 +0800

    save work

--- src/chainseye.com/router/router.go
+++ src/chainseye.com/router/router.go
@@ -11,16 +11,26 @@ import (
 //SetRouter gin框架设置路由
 func SetRouter() {
 
-	gin.SetMode(gin.ReleaseMode)
+	//gin.SetMode(gin.ReleaseMode)
 
-	//ginRouter := gin.Default()
-	ginRouter := gin.New()
+	ginRouter := gin.Default()
+	//ginRouter := gin.New()
 
 	ginRouter.Use(MiddleWare)
 
-	ginRouter.Static("./src/chainseye.com/view/static", "ginStatic")
-	ginRouter.LoadHTMLGlob("./src/chainseye.com/view/templates/*")
+	//ginRouter.StaticFS("/index", http.Dir("."))
+	//router.Static("/i", "/bin")
 
+	ginRouter.Static("/index", "./src/chainseye.com/view/static/skin/css")
+	ginRouter.LoadHTMLGlob("src/chainseye.com/view/templates/*")
+
+	displayPage(ginRouter)
+	//makeApi(ginRouter)
+
+	ginRouter.Run(":8099")
+}
+
+func displayPage(ginRouter *gin.Engine) {
 	ginRouter.GET("/", func(context *gin.Context) {
 
 		context.HTML(http.StatusOK, "cWF_index.tmpl", nil)
@@ -29,12 +39,16 @@ func SetRouter() {
 
 	ginRouter.GET("/chains_back/", func(context *gin.Context) {
 
-		context.HTML(http.StatusOK, "cWB_portal.tmpl", nil)
+		context.HTML(http.StatusOK, "cWB_portal.tmpl", gin.H{
+			"info": "---  from router  ---",
+		})
 
 	})
-
-	ginRouter.Run(":8099")
 }
+
+// func makeApi(ginRouter *gin.Engine) {
+
+// }
 
 //MiddleWare 中间件
 func MiddleWare(c *gin.Context) {
