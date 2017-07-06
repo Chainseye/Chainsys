8830cf2b2d36a459d7013019d1fbcacbd671c15e
liph <liph@hiscene.com>
2017-07-06 15:06:06 +0800

    save work

--- src/chainseye.com/static/component/chainsBall/chainsBall.js
+++ src/chainseye.com/static/component/chainsBall/chainsBall.js
@@ -1,45 +1,31 @@
 ;(function($, window, document, undefined) {
 
     // 定义构造函数
-    var ChainsBar = function(ele, opt) {
+    var ChainsBall = function(ele, opt) {
         this.$ele = ele;
         this.options = {
-            "width": "",
-            "height": "20px",
-            "maxCount": 5,
-            "content": ["news1", "news2", "news3", "news4"],
-            "direction": "left"
+            "container": ""
         };
         this.options = $.extend({}, this.options, opt)
     };
 
     // 定义方法
-    ChainsBar.prototype = {
+    ChainsBall.prototype = {
         init: function() {
             var that = this;
-            that.createChainsBarDom();
+            that.createChainsBall();
         },
-        createChainsBarDom: function() {
+        createChainsBall: function() {
             var that = this;
-            var newsItemStr = "";
-            that.options.content.forEach(function(val, index) {
-                newsItemStr += "<div class='chainsBar-newsItem'><p>" + index + "==>" + val + "</p></div>";
-            });
-            that.$ele.html("<div class='chainsBar-content'>" + newsItemStr + "</div>");
-            that.addBaseChainsBarCss();
-        },
-        addBaseChainsBarCss: function() {
-            var that = this;
-
         }
     };
 
     // 使用对象
-    $.fn.chainsBar = function(options) {
+    $.fn.chainsBall = function(options) {
         // 创建实体
-        var chainsBar = new ChainsBar(this, options);
+        var chainsBall = new ChainsBall(this, options);
 
         // 调用初始化方法
-        return chainsBar.init();
+        return chainsBall.init();
     }
 })(jQuery, window, document);
