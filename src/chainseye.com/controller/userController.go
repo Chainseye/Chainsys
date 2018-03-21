d5fd09b336b965ce15fc5fb42f4d4a4e972a82b9
Chainseye <wuqujiawuliao@sina.com>
2018-03-22 01:09:20 +0800

    rm sql & update websocket

--- src/chainseye.com/controller/userController.go
+++ src/chainseye.com/controller/userController.go
@@ -3,28 +3,27 @@ package controller
 import (
 	"net/http"
 
-	"chainseye.com/service"
-
 	"github.com/gin-gonic/gin"
 )
 
 //GetUserInfoByUID 根据UID获取基本信息
 func GetUserInfoByUID(context *gin.Context) {
 
-	ur, err := service.GetUserInfoByUID(1)
-	if err != nil {
-		context.JSON(http.StatusOK, gin.H{
-			"retCode": 1001,
-			"info":    err,
-		})
-		return
-	}
-	userData := map[string]interface{}{
-		"mobile":   ur.Mobile,
-		"password": ur.Password,
-	}
+	// ur, err := service.GetUserInfoByUID(1)
+	// if err != nil {
+	// 	context.JSON(http.StatusOK, gin.H{
+	// 		"retCode": 1001,
+	// 		"info":    err,
+	// 	})
+	// 	return
+	// }
+	// userData := map[string]interface{}{
+	// 	"mobile":   ur.Mobile,
+	// 	"password": ur.Password,
+	// }
 	context.JSON(http.StatusOK, gin.H{
 		"retCode": 0,
-		"info":    userData,
+		"info": nil,
+		// "info":    userData,
 	})
 }
